9c7a7adb8768d75eb54284edc48d17d94f925100
Priscila Lima <Priscila Lima>
2016-07-01 20:31:52 -0300

    telas inicial e create account criadas (layout)

--- FriendsInTown/app/src/main/java/alexsander/com/br/friendsintown/MainActivity.java
+++ FriendsInTown/app/src/main/java/alexsander/com/br/friendsintown/MainActivity.java
@@ -1,7 +1,11 @@
 package alexsander.com.br.friendsintown;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       // Button createAccountButton = (Button) findViewById(R.id.btnCreateAccount);
+      //  createAccountButton.setOnClickListener(new View.OnClickListener() {
+       //     @Override
+       //     public void onClick(View view) {
+
+      //      }
+      //  });
     }
+
+
 }
