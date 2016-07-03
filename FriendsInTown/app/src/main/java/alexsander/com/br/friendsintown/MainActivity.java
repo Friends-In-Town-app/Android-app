35c66ea0defa44dd0fe0981d41c48e3ecebbb4f5
Priscila Lima <Priscila Lima>
2016-07-02 21:17:26 -0300

    teste commit

--- FriendsInTown/app/src/main/java/alexsander/com/br/friendsintown/MainActivity.java
+++ FriendsInTown/app/src/main/java/alexsander/com/br/friendsintown/MainActivity.java
@@ -4,24 +4,29 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+
+    private Button createAccountButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       // Button createAccountButton = (Button) findViewById(R.id.btnCreateAccount);
-      //  createAccountButton.setOnClickListener(new View.OnClickListener() {
-       //     @Override
-       //     public void onClick(View view) {
+        createAccountButton = (Button) findViewById(R.id.btnCreateAc);
+        createAccountButton.setOnClickListener(this);
 
-      //      }
-      //  });
     }
 
+    public void onClick(View v) {
+        if (v == createAccountButton){
+            startActivity(new Intent(MainActivity.this, CreateAccountActivity.class));
 
+        }
 }
+
+
+        }
